2ad61f3807c24557905abf0f4e9bc785803273cd
minsung <jms5049@naver.com>
2017-08-14 18:20:14 +0900

    webView

---
+++ app/src/main/java/com/example/joh/webview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.joh.webview;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = (WebView)
+                findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:(function(){ " +
+                        "document.getElementsByClassName('mwUI_is_fixed_wrap')[0].style.display='none'; " +
+                        "document.getElementsByClassName('mwUI_is_fixed_wrap_sub-top-container')[0].style.display='none'; " +
+                        "document.getElementsByClassName('floatingMenu')[0].style.display='none'; " +
+                        "document.getElementsByClassName('banner-container')[0].style.display='none'; " +
+                        "document.getElementsByClassName('sponsor-list')[0].style.display='none'; " +
+                        "document.getElementsByClassName('footer')[0].style.display='none'; " +
+                        "})()");
+                webView.setVisibility(View.VISIBLE);
+            }
+        });
+
+        webView.loadUrl("https://www.pyeongchang2018.com/ko/schedule");
+    }
+}
